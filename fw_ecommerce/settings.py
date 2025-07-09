41e90137547276d4a28c474d0706cee230fb4067
andreapetrucci <a.petru01@hotmail.com>
2025-07-09 17:37:04 +0200

    fix db

--- fw_ecommerce/settings.py
+++ fw_ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.getenv("DEBUG", "False") == "True"
+SECRET_KEY = "3a2$))239vpmt5s8k+!l$ccm$m-1v%@!ny+cl!5o@bxatnpl^a"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", default="en-us")
+TIME_ZONE = os.environ.get("TIME_ZONE", default="UTC")
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -79,24 +84,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'fw_ecommerce.wsgi.application'
 
-
-# Database
-# Controlla se DATABASE_URL è presente (cioè se siamo su Railway)
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    # Siamo in locale → usa SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+    'default': dj_database_url.parse("postgresql://postgres:wzCCVUmFqJJCgkqzPDbOsALnfBrqknde@centerbeam.proxy.rlwy.net:52353/railway")
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -119,10 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = 'it-IT'
-
-TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
